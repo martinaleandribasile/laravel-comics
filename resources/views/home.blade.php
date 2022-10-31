b4d0fd9c85edd57b1a4c696cd647acc79356e9e4
MartinaBasile <martinaleandribasile@gmail.com>
2022-10-31 10:18:53 +0100

    add shop section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,5 +10,40 @@
                 </div>
             @endforeach
         </div>
+        <button class="btn btn-primary">LOAD MORE</button>
+    </div>
+    <div class="main-blu">
+        <div class="boxcontainer shop">
+            <div class="shopcard">
+                <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="">
+                <a href="">
+                    <p>DIGITAL COMICS</p>
+                </a>
+            </div>
+            <div class="shopcard">
+                <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
+                <a href="">
+                    <p>DC MERCHANDISE</p>
+                </a>
+            </div>
+            <div class="shopcard">
+                <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
+                <a href="">
+                    <p>SUBSCRIPTION</p>
+                </a>
+            </div>
+            <div class="shopcard">
+                <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
+                <a href="">
+                    <p>COMIC SHOP LOCATION</p>
+                </a>
+            </div>
+            <div class="shopcard">
+                <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="">
+                <a href="">
+                    <p>DC POWER VISA</p>
+                </a>
+            </div>
+        </div>
     </div>
 @endsection
